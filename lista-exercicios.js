e443be08ed56f085d268f8361b3dded695ee7b3c
rafabrazil <rafael.h@outlook.com>
2022-06-07 15:50:45 -0300

    Lista finalizada*

--- lista-exercicios.js
+++ lista-exercicios.js
@@ -69,3 +69,153 @@ const livro = new Livro(
     '16 de maio de 2022',
     ['SKU 26742287', 'Acabamento: Brochura', 'Dimensão: 14 x 21', 'Livro em bom estado de conservação', 'Possui vários grifos a lápis', 'Páginas: 96']
 )
+
+// 2- Faça um código em JS que realize a operação desejada entre os números 33 e 5,
+// conforme um operador informado.
+
+let operador = '-' //o operador deverá ser alterado para testar cada caso
+let resultado = 0
+
+switch (operador) {
+    case '-':
+        resultado = 33 - 5
+        console.log('O resultado da operação de 33 - 5 é: ', resultado)
+        break;
+    case '+':
+        resultado = 33 + 5
+        console.log('O resultado da operação de 33 + 5 é: ', resultado)
+        break;
+    case '*':
+        resultado = 33 * 5
+        console.log('O resultado da operação de 33 * 5 é: ', resultado)
+        break;
+    case '/':
+        resultado = 33 / 5
+        console.log('O resultado da operação de 33 / 5 é: ', resultado)
+        break;
+    case '%':
+        resultado = 33 % 5
+        console.log('O resultado da operação de 33 % 5 é: ', resultado)
+        break;
+    default:
+        console.log('Operador inválido')
+
+}
+
+// 3- Retornar todas as frutas que tenham a letra "o"
+
+const frutas = ["Abacaxi", "Uva", "Maçã", "Goiaba", "Abacate", "Acerola"]
+
+const frutasComO = frutas.filter((fruta) => { return fruta.includes('o') })
+
+console.log(frutasComO)
+
+// 4- Faça um código em JS que informe o total de horas trabalhadas no dia de uma funcionária.
+
+const calculaHoraTrabalhada = (horaTrablhada) => {
+    typeof (horaTrablhada) === 'number' ? (
+        horaTrablhada < 6 ?
+            console.log('Você trabalhou ', horaTrablhada, ' horas') :
+            console.log('Você trabalhou ', horaTrablhada - 1, ' horas')) :
+        console.log('O valor informado não é valido')
+}
+
+// 5- Faça um código em JS que realize a soma dos números pares pertencentes ao intervalo de 1 a 100 e retorne o resultado da soma.
+
+const somarNumPar = (inicio = 1, fim = 100) => {
+    let intervalo = []
+    for (let i = 0; i <= (fim - inicio); i++) {
+        (inicio + i) % 2 === 0 && (intervalo.push(inicio + i));
+    }
+
+    const soma = intervalo.reduce((somatorio, numero) => {
+        return somatorio + numero
+    })
+    // console.log(intervalo)
+    console.log(soma)
+}
+
+// 6- Faça um código em JS que retorne os números do array abaixo de forma decrescente.
+
+const inverteArray = (arr) => {
+    let invertido = []
+
+    for (let i = arr.length; i > 0; i--) {
+        invertido.push(arr[i - 1])
+
+    }
+    console.log(invertido)
+}
+
+// 7- Faça um código em JS que retorne quantos dias existem entre as datas de início e fim
+
+const listaDeDatas = [{ dateStart: '12/01/2022', dateEnd: '12/03/2022' },
+{ dateStart: '27/03/2022', dateEnd: '11/04/2022' },
+{ dateStart: '02/04/2022', dateEnd: '22/04/2022' },
+{ dateStart: '11/05/2022', dateEnd: '12/05/2022' }]
+
+const calcularDiferencaDeDias = (listaDeDatas) => {
+    // For usado para converter a data para um formato aceito pelo Date()
+    for (let i = 0; i < listaDeDatas.length; i++) {
+        listaDeDatas[i].dateEnd = listaDeDatas[i].dateEnd.split('/').reverse().join('/')
+        listaDeDatas[i].dateStart = listaDeDatas[i].dateStart.split('/').reverse().join('/')
+
+    }
+
+    const intervaloDeDias = listaDeDatas.map((data) => {
+        let dia = 3600000 * 24
+        let dataInicio = new Date(data.dateStart)
+        let dataFim = new Date(data.dateEnd)
+        return (dataFim - dataInicio) / dia
+    })
+
+    console.log(intervaloDeDias)
+}
+
+// 8- Faça um código em JS que estruture os dados, calcule e retorne o resultado do somatório da lista de compras
+
+function Carrinho(itens, codigoConsultor, formaPagamento,) {
+    this.itens = itens,
+    this.codigoConsultor = codigoConsultor,
+    this.formaPagamento = formaPagamento
+
+}
+
+const car = new Carrinho([{
+        nome: 'jaqueta trucker jeans manga longa com forro de pelo azul médio g',
+        valorUnitario: 249.99,
+        qunatidade: 1,
+        desconto: 0,
+        valorTotal: 249.99,
+        vendidoPor: 'CeA'
+    },
+    {
+        nome: 'mochilete média ever after high 17z',
+        valorUnitario: 399.90,
+        qunatidade: 1,
+        desconto: 337,
+        valorTotal: 62.90,
+        vendidoPor: 'Mundo Bag'
+    },
+    {
+        nome: 'oculos de sol aviador ace dourado unico',
+        valorUnitario: 69.99,
+        qunatidade: 2,
+        desconto: 0,
+        valorTotal: 139.98,
+        vendidoPor: 'CeA'
+    },
+    {
+        nome: 'suéter de pelúcia sherpa com zíper bege m',
+        valorUnitario: 199.99,
+        qunatidade: 1,
+        desconto: 0,
+        valorTotal: 199.99,
+        vendidoPor: 'CeA'
+    }
+    ],'',''
+)
+
+const somarCarrinho = (carr) => {
+    return carr.itens.map(item => item.valorTotal).reduce((pre, curr) => pre + curr)
+}
